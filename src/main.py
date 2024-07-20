9cacbdf41caf11ff10a204737159edca9a0bba45
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2024-07-20 00:45:34 -0700

    vscode integration

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 # Run application
 from src.app.login import Login
 from src.structures.user import UserManager
+import sys
+import os
+
+# For VSCode
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def main():
