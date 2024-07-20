5d590d4a32c4aeb713302658c0ce195777358068
luca <lfbianchini@dons.usfca.edu>
2024-07-19 17:36:48 -0700

    Back button for login

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 # Run application
-from login import login
+from login import Login
 from user_manager import UserManager
 
 
 def main():
     user_manager = UserManager()
 
-    login(user_manager)
+    login = Login(user_manager)
+    login.login()
 
 
 if __name__ == "__main__":
