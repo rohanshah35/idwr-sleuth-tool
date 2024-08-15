29ea3012f6e02515d14ff03873ee8aadfd231c23
Rohan Shah <66500419+rohanshah35@users.noreply.github.com>
2024-08-15 09:15:13 -0700

    Loading screen change

--- src/controllers/loading_screen.py
+++ src/controllers/loading_screen.py
@@ -6,11 +6,16 @@ class LoadingScreenController:
     def __init__(self, app):
         self.app = app
         self.frame = ttk.Frame(app.frame)
-        self.label = ttk.Label(self.frame, text="Loading...", font=("Helvetica", 16, "bold"))
+        self.label = ttk.Label(self.frame, text="LOADING...", font=("Helvetica", 16, "bold"))
         self.label.pack(expand=True)
+
+        # self.progress = ttk.Progressbar(self.frame, mode="indeterminate", length=300)
+        # self.progress.pack(pady=10)
 
     def show(self):
         self.frame.pack(fill=tk.BOTH, expand=True)
+        # self.progress.start()
 
     def hide(self):
+        # self.progress.stop()
         self.frame.pack_forget()
